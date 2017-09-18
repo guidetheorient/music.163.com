6baa55f1d85ed94499b9c9a88a4bb2c798fbe6cd
guidetheorient <chrisji0518@outlook.com>
2017-09-18 19:12:20 +0800

    建立歌曲数据库，增加中英文歌词排版

--- src/js/play.js
+++ src/js/play.js
@@ -1,38 +1,63 @@
 $(function(){
   $.get('/src/lyric.json').then(function(response){
     let lyric = response.lrc.lyric
-    let array = lyric.split('\n')
+    let tlyric = response.tlyric.lyric
     
-    let regexp = /\[\d+:\d+\.\d+\]/
-    //去掉歌词上传者的注释信息
-    array.map(function(ele,index){
-      if(!regexp.test(ele) || ele == ''){
-        array.splice(index,1)
-      }
-    })
+    let originLrc = generateLrc(lyric)
+    let translateLrc = generateLrc(tlyric)
 
-    //将时间和歌词分开
-    let regexp2 = /\[(.+)\](.*)/
-    array = array.map(function(ele){
-      let matches = regexp2.exec(ele)
-      if(!!matches){
-        return {time:matches[1],line:matches[2]}
-      }
+    //合并中英文数组
+    originLrc.map((ele,index)=>{
+      let originEle = ele
+      let originIndex = index
+      translateLrc.forEach((ele,index)=>{
+        if(ele.time === originEle.time){
+          originLrc[originIndex].line2 = ele.line
+        }
+      })
     })
-    
+    console.log(originLrc)
+
     //生成歌词dom节点
     let $lrc = $('.lrc')
-    array.map(function(ele,index){
+    originLrc.map((ele,index)=>{
       let $p = $('<p></p>')
-      console.log(ele)
-      $p.attr({'data-time':ele.time,class:"lrc-line"}).text(ele.line)
+      let $span1 = $('<span></span>')
+      let $span2 = $('<span></span>')
+      
+      $span1.attr({class:"origin-lrc-line"}).text(ele.line)
+      $span2.attr({class:"translate-lrc-line"}).text(ele.line2)
+      
+      $p.attr({'data-time':ele.time,class:"lrc-line"}).append($span1,$span2)
+      
       $lrc.append($p)
     })
+
+
+    function generateLrc(lrc){
+      let array = lrc.split('\n')
+      let regexp = /\[\d+:\d+\.\d+\]/
+
+      //去掉歌词上传者的注释信息
+      array = array.filter(function(ele){
+        return !(!regexp.test(ele) || ele == '')
+      })
+
+      //将时间和歌词分开
+      let regexp2 = /\[(.+)\](.*)/
+      array = array.map(function(ele){
+        let matches = regexp2.exec(ele)
+        if(!!matches){
+          return {time:matches[1],line:matches[2]}
+        }
+      })
+      return array
+    }
   })
   
   let audio = document.createElement('audio')
   let $disc = $('#app .disc')
-  audio.src = 'http://dl.stream.qqmusic.qq.com/C400000fvI5R4cjyYA.m4a?fromtag=38&vkey=25EE4BEB308BB660E8DDB55D3610C36FB16152F260B1BBF7689FFF6FCE8B17B37E810C34A63223AF766B5E1E0A2410E3C0F0DDBAD442EF23&guid=6400546447'
+  audio.src = ''
   audio.oncanplay = function(){
     audio.play()
     $disc.addClass('playing')
