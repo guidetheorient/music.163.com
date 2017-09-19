0a31a5689c98b5c778d8688c16db7645b91e2c66
guidetheorient <chrisji0518@outlook.com>
2017-09-19 15:02:59 +0800

    播放界面从json中获取对应数据存放地址

---
+++ src/js/index.js
@@ -0,0 +1,45 @@
+$(function(){
+  $.get('/src/songsDB.json',function(response){
+    let songDB = response
+    let $latestMusic = $('.latest-music')
+    let $musicList = $('<ol></ol>')
+
+    songDB.forEach((ele,index)=>{
+      $li = $(`
+        <li>
+          <h3 class="song-name">${ele.songName}</h3>
+          <p class="song-info">  
+            ${ele.songAuthor} - ${ele.album}
+          </p>
+          <a class="play-btn" href="#">
+              <svg class="icon" aria-hidden="true">
+                <use xlink:href="#icon-play"></use>
+              </svg>
+          </a>
+        </li>
+      `)
+
+      //添加超清标记
+      if(ele.sq === true){
+        $li.find('.song-info').prepend($(`
+          <svg class="icon sq" aria-hidden="true">
+            <use xlink:href="#icon-sq"></use>
+          </svg>
+        `))
+      }
+      
+      $musicList.append($li)
+    })
+    //最新音乐
+    $latestMusic.append($musicList)
+
+    $latestMusic.find('.loading').remove()
+
+    $musicList.on('click','li',function(){
+      let $this = $(this)
+      let index = $this.index()
+      window.location.href = `./play.html?id=${index}`
+    })
+  })
+
+})
--- src/js/play.js
+++ src/js/play.js
@@ -1,86 +1,126 @@
 $(function(){
-  $.get('/src/lyric.json').then(function(response){
-    let lyric = response.lrc.lyric
-    let tlyric = response.tlyric.lyric
+  let search = window.location.search
+  let id = Number(search.match(/\?id=(\d+)/)[1])
+  
+  $.get('/src/songsDB.json').then(function(response){
+    let song = response[id]
+    console.log(song)
+    let $songBg = $('#app .song-bg')
+    let $cover = $('<img/>')
+    let $defaultCover = $('.default-cover')
+    var offset
+
+    //背景图片
+    $songBg.css({'background-image':`url(${song.backgroundImgUrl})`})
     
-    let originLrc = generateLrc(lyric)
-    let translateLrc = generateLrc(tlyric)
+    //专辑封面
+    $cover.attr({'src':song.coverUrl,'class':'cover'}).appendTo($defaultCover)
 
-    //合并中英文数组
-    originLrc.map((ele,index)=>{
-      let originEle = ele
-      let originIndex = index
-      translateLrc.forEach((ele,index)=>{
-        if(ele.time === originEle.time){
-          originLrc[originIndex].line2 = ele.line
+    //歌手，专辑信息
+    let $songInfo = $('.song-info')
+    let $songTitle = $(`
+      <div class="title">
+        <span class="song-name">${song.songName}</span>
+        <span>-</span>
+        <span class="song-author">${song.album}</span>
+      </div>
+    `)
+    $songTitle.prependTo($songInfo)
+
+    //歌词
+    if(!song.hasLrc){
+
+    }else{
+      $.get(`/src/lrc/${id+1}.json`).then(function(response){
+        let lyric = response.lrc.lyric
+        let tlyric = response.tlyric.lyric
+        
+        let originLrc = generateLrc(lyric)
+        let translateLrc = generateLrc(tlyric)
+    
+        //合并中英文数组
+        originLrc.map((ele,index)=>{
+          let originEle = ele
+          let originIndex = index
+          translateLrc.forEach((ele,index)=>{
+            if(ele.time === originEle.time){
+              originLrc[originIndex].line2 = ele.line
+            }
+          })
+        })
+    
+        //生成歌词dom节点
+        let $lrcWrap = $('.lrc-wrap')
+  
+        let $lrc = $('<div class="lrc"></div>')
+        originLrc.map((ele,index)=>{
+          let $p = $('<p></p>')
+          let $span1 = $('<span></span>')
+          let $span2 = $('<span></span>')
+          
+          $span1.attr({class:"origin-lrc-line"}).text(ele.line)
+          $span2.attr({class:"translate-lrc-line"}).text(ele.line2)
+          
+          $p.attr({'data-time':ele.time,class:"lrc-line"}).append($span1,$span2)
+          
+          $lrc.append($p)
+        })
+  
+        $lrcWrap.append($lrc)
+        offset = $('.lrc-line').outerHeight(true)
+        
+        function generateLrc(lrc){
+          let array = lrc.split('\n')
+          let regexp = /\[\d+:\d+\.\d+\]/
+    
+          //去掉歌词上传者的注释信息
+          array = array.filter(function(ele){
+            return !(!regexp.test(ele) || ele == '')
+          })
+    
+          //将时间和歌词分开
+          let regexp2 = /\[(.+)\](.*)/
+          array = array.map(function(ele){
+            let matches = regexp2.exec(ele)
+            if(!!matches){
+              return {time:matches[1],line:matches[2]}
+            }
+          })
+          return array
         }
       })
-    })
-    console.log(originLrc)
-
-    //生成歌词dom节点
-    let $lrc = $('.lrc')
-    originLrc.map((ele,index)=>{
-      let $p = $('<p></p>')
-      let $span1 = $('<span></span>')
-      let $span2 = $('<span></span>')
-      
-      $span1.attr({class:"origin-lrc-line"}).text(ele.line)
-      $span2.attr({class:"translate-lrc-line"}).text(ele.line2)
-      
-      $p.attr({'data-time':ele.time,class:"lrc-line"}).append($span1,$span2)
-      
-      $lrc.append($p)
-    })
-
-
-    function generateLrc(lrc){
-      let array = lrc.split('\n')
-      let regexp = /\[\d+:\d+\.\d+\]/
-
-      //去掉歌词上传者的注释信息
-      array = array.filter(function(ele){
-        return !(!regexp.test(ele) || ele == '')
-      })
-
-      //将时间和歌词分开
-      let regexp2 = /\[(.+)\](.*)/
-      array = array.map(function(ele){
-        let matches = regexp2.exec(ele)
-        if(!!matches){
-          return {time:matches[1],line:matches[2]}
-        }
-      })
-      return array
     }
+    console.log(offset)
+    //音频
+    let audio = document.createElement('audio')
+    let $disc = $('#app .disc')
+    audio.src = song.url
+    audio.oncanplay = function(){
+      audio.play()
+      $disc.addClass('playing')
+    }
+    audio.onended = function(){
+      $disc.addClass('paused').removeClass('playing')
+    }
+
+    //歌词滚动
+    audio.ontimeupdate = function(){
+      let currentTime = audio.currentTime
+    }
+
+    let $playBtn = $('.disc-container .play-btn')
+    let $pauseBtn = $('.disc .pause-btn')
+    $playBtn.click(function(){
+      audio.play()
+      $disc.removeClass('paused').addClass('playing')    
+    })
+    $pauseBtn.click(function(){
+      audio.pause()
+      $disc.addClass('paused').removeClass('playing')
+    })
   })
   
-  let audio = document.createElement('audio')
-  let $disc = $('#app .disc')
-  audio.src = ''
-  audio.oncanplay = function(){
-    audio.play()
-    $disc.addClass('playing')
-    scrollLrc()
-  }
-
-  function scrollLrc(){
-    let currentTime = audio.currentTime
-    
-  }
-
-
-
-  let $playBtn = $('.disc-container .play-btn')
-  let $pauseBtn = $('.disc .pause-btn')
-  $playBtn.click(function(){
-    audio.play()
-    $disc.removeClass('paused').addClass('playing')    
-  })
-  $pauseBtn.click(function(){
-    audio.pause()
-    $disc.addClass('paused').removeClass('playing')
-  })
+  
 
 
 
