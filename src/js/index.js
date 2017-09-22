6d01f955f52f771ea5b51d193589ae64fe024783
guidetheorient <chrisji0518@outlook.com>
2017-09-22 22:56:29 +0800

    完成index.html tab 2

--- src/js/index.js
+++ src/js/index.js
@@ -7,10 +7,12 @@ $(function(){
     songDB.forEach((ele,index)=>{
       $li = $(`
         <li>
-          <h3 class="song-name">${ele.songName}</h3>
-          <p class="song-info">  
-            ${ele.songAuthor} - ${ele.album}
-          </p>
+          <div class="song">
+            <h3 class="song-name">${ele.songName}</h3>
+            <p class="song-info">  
+              ${ele.songAuthor} - ${ele.album}
+            </p>
+          </div>
           <a class="play-btn" href="#">
               <svg class="icon" aria-hidden="true">
                 <use xlink:href="#icon-play"></use>
@@ -31,15 +33,97 @@ $(function(){
       $musicList.append($li)
     })
     //最新音乐
-    $latestMusic.append($musicList)
-
-    $latestMusic.find('.loading').remove()
+    $latestMusic.append($musicList).find('.loading').remove()
 
     $musicList.on('click','li',function(){
-      let $this = $(this)
-      let index = $this.index()
+      let index = $(this).index()
       window.location.href = `./play.html?id=${index}`
     })
+
+    //tab切换
+    $('.index-nav').on('click','.tab-header>li',function(e){
+      let $li = $(e.currentTarget)
+      $li.addClass('active').siblings().removeClass('active')
+      let index = $li.index()
+      $li.trigger('tabSwitch',index)
+      $('.tab-content > li').eq(index).addClass('active').siblings().removeClass('active')
+    })
+    
+    //trigger指定或冒泡到祖先元素
+    $('.index-nav').on('tabSwitch',function(e,index){
+      let $liContent = $('.tab-content>li').eq(index)
+      
+      if($liContent.attr('data-isLoaded')==='yes') {
+        return
+      };
+      if(index ===1){
+        $.get('./src/hotlist.json').then(function(response){
+          let songDB = response
+          let $musicList = $('<ol></ol>')
+          let $hotListTab = $('.hot-list-tab')
+          
+          function orderNumber(index){
+            let number = index+1
+            if(Number(number)>0&&Number(number)<10){
+              return '0'+number
+            }else{
+              return ''+number
+            }
+          }
+
+          songDB.forEach((ele,index)=>{
+            $li = $(`
+              <li>
+                <h3 class="order-number">${orderNumber(index)}</h3>
+                <div class="song">
+                  <h3 class="song-name">${ele.songName}</h3>
+                  <p class="song-info">  
+                    ${ele.songAuthor} - ${ele.album}
+                  </p>
+                  <a class="play-btn" href="#">
+                      <svg class="icon" aria-hidden="true">
+                        <use xlink:href="#icon-play"></use>
+                      </svg>
+                  </a>
+                </div>
+              </li>
+            `)
+
+            //添加超清标记
+            if(ele.sq === true){
+              $li.find('.song-info').prepend($(`
+                <svg class="icon sq" aria-hidden="true">
+                  <use xlink:href="#icon-sq"></use>
+                </svg>
+              `))
+            }
+            
+            $musicList.append($li)
+          })
+
+          $hotListTab.append($musicList).find('.loading').remove()
+          $musicList.on('click','li',function(){
+            let index = $(this).index()
+            window.location.href = `./play.html?id=${index}`
+          })
+          $liContent.attr('data-isLoaded','yes')
+        })
+      }else if(index ===2){
+        $.get('./src/search.json').then(function(response){
+          
+
+
+
+
+
+
+
+          $liContent.attr('isLoaded','yes')
+        })
+      }
+    })
+
+
   })
 
 })
