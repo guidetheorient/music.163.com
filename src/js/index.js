227c3e9bbcfdc8efa098774fbeaf1def16350382
guidetheorient <chrisji0518@outlook.com>
2017-09-27 15:59:46 +0800

    完成所有功能

--- src/js/index.js
+++ src/js/index.js
@@ -1,13 +1,19 @@
 $(function(){
+  //生成推荐歌单
   $.get('/src/recommendList.json').then(function(response){
     let recommendList = response
-    console.log(recommendList)
     let $recommendList = $('.recommend-playlists')
     let $ul = $('<ul></ul>')
 
     recommendList.map(function(e,index){
       $li = `
         <li data-rId=${e.rId}>
+          <span class="view-time">
+            <svg class="icon headphone" aria-hidden="true">
+                <use xlink:href="#icon-headphone"></use>
+            </svg>
+            <span class="views">${e.rPlayTimes}</span>
+          </span>
           <img src="${e.rCoverUrl}" alt="">
           <p class="playlists-info">${e.rTitle}</p>
         </li>
@@ -17,17 +23,15 @@ $(function(){
     $recommendList.append($ul)
     $recommendList.find('ul').on('click','li',function(e){
       let rid = $(e.currentTarget).attr('data-rId')
-      console.log(rid)
       window.location.href = `./playlist.html?rid=${rid}`
-    })    
+    }) 
   })
   
+  //生成最新音乐
   $.get('//owf5g9dnv.bkt.clouddn.com/songsDB.json?v=20170922',function(response){
     let songDB = response
     let $latestMusic = $('.latest-music')
     let $musicList = $('<ol></ol>')
-    // alert('whf23l',songDB)
-    
     songDB.forEach((ele,index)=>{
       $li = $(`
         <li>
@@ -56,229 +60,277 @@ $(function(){
       
       $musicList.append($li)
     })
-    
-    //最新音乐
     $latestMusic.append($musicList).find('.loading').remove()
-
     $musicList.on('click','li',function(){
       let index = $(this).index()
       window.location.href = `./play.html?id=${index}`
     })
+  })
 
-    //tab切换
-    $('.index-nav').on('click','.tab-header>li',function(e){
-      let $li = $(e.currentTarget)
-      $li.addClass('active').siblings().removeClass('active')
-      let index = $li.index()
-      $li.trigger('tabSwitch',index)
-      $('.tab-content > li').eq(index).addClass('active').siblings().removeClass('active')
-    })
+  //tab切换
+  $('.index-nav').on('click','.tab-header>li',function(e){
+    let $li = $(e.currentTarget)
+    let index = $li.index()    
+    $li.addClass('active').siblings().removeClass('active')
+    $li.trigger('tabSwitch',index)
+    $('.tab-content > li').eq(index).addClass('active').siblings().removeClass('active')
+  })
+  
+  
+  //trigger指定或冒泡到祖先元素
+  $('.index-nav').on('tabSwitch',function(e,index){
+    let $liContent = $('.tab-content>li').eq(index)
     
-    //trigger指定或冒泡到祖先元素
-    $('.index-nav').on('tabSwitch',function(e,index){
-      let $liContent = $('.tab-content>li').eq(index)
-      
-      if($liContent.attr('data-isLoaded')==='yes') {
-        return
-      };
-      if(index ===1){
-        $.get('./src/hotlist.json').then(function(response){
-          let songDB = response
-          let $musicList = $('<ol></ol>')
-          let $hotListTab = $('.hot-list-tab')
-          
-          function orderNumber(index){
-            let number = index+1
-            if(Number(number)>0&&Number(number)<10){
-              return '0'+number
-            }else{
-              return ''+number
-            }
-          }
+    //已经加载过的，return掉
+    if($liContent.attr('data-isLoaded')==='yes') {
+      return
+    };
 
-          songDB.forEach((ele,index)=>{
-            $li = $(`
-              <li>
-                <h3 class="order-number">${orderNumber(index)}</h3>
-                <div class="song">
-                  <h3 class="song-name">${ele.songName}</h3>
-                  <p class="song-info">  
-                    ${ele.songAuthor} - ${ele.album}
-                  </p>
-                  <a class="play-btn" href="#">
-                      <svg class="icon" aria-hidden="true">
-                        <use xlink:href="#icon-play"></use>
-                      </svg>
-                  </a>
-                </div>
-              </li>
-            `)
-
-            //添加超清标记
-            if(ele.sq === true){
-              $li.find('.song-info').prepend($(`
-                <svg class="icon sq" aria-hidden="true">
-                  <use xlink:href="#icon-sq"></use>
-                </svg>
-              `))
-            }
-            
-            $musicList.append($li)
-          })
-
-          $hotListTab.append($musicList).find('.loading').remove()
-          $musicList.on('click','li',function(){
-            let index = $(this).index()
-            window.location.href = `./play.html?id=${index}`
-          })
-          $liContent.attr('data-isLoaded','yes')
-        })
-      }else if(index ===2){
-
-        let $searchBox = $('.search input')
-        let timer
-        let $emptyInput = $('.search .x-icon')
-        let $hotSearch = $('.hot-search')
-        let $searchHistory = $('.search-history')
-        let $syncSearch = $('.sync-search')
-        let $searchWhat = $('.search-what')
-        let regexp = /^\[(.*)\]$/
-        //组织form表单默认提交，储存查询历史
-        $('.search form').keypress(function(e){
-          if(e.keyCode==13){
-            let value = $searchBox.val()
-            e.preventDefault();
-            if(localStorage.getItem('searchHistory')===null||!(regexp.test(localStorage.getItem('searchHistory')))){
-              localStorage.setItem('searchHistory','[]')
-            }
-            let item = localStorage.getItem('searchHistory')
-            let itemArray = item.match(regexp)[1].split(',')
-            if(value !==''){
-              itemArray.push(value)
-              localStorage.setItem('searchHistory','['+itemArray+']')
-              generateHistory(itemArray)
-            }
-          }
-        })
-
-
-        // generateHistory()
-        function generateHistory(itemArray){
-          console.log(itemArray)
-          let $ul = $('<ul></ul>')
-          if(itemArray.length < 1) return;
-          itemArray.map(function(ele){
-            if(ele==='')return
-            let $li = `
-              <li class="item">
-                <svg class="icon history-icon" aria-hidden="true">
-                  <use xlink:href="#icon-history"></use>
-                </svg>
-                <div class="history">
-                    <span>${ele}</span>
-                    <svg class="icon close-icon" aria-hidden="true">
-                      <use xlink:href="#icon-close"></use>
+    //index= 1热歌榜
+    if(index ===1){
+      $.get('./src/hotlist.json').then(function(response){
+        let songDB = response
+        let $musicList = $('<ol></ol>')
+        let $hotListTab = $('.hot-list-tab') 
+        songDB.forEach((ele,index)=>{
+          $li = $(`
+            <li>
+              <h3 class="order-number">${orderNumber(index)}</h3>
+              <div class="song">
+                <h3 class="song-name">${ele.songName}</h3>
+                <p class="song-info">  
+                  ${ele.songAuthor} - ${ele.album}
+                </p>
+                <a class="play-btn" href="#">
+                    <svg class="icon" aria-hidden="true">
+                      <use xlink:href="#icon-play"></use>
                     </svg>
-                </div>
-              </li>
-            `
-            $ul.append($li)
-          })
-          $searchHistory.html($ul)
-          $searchHistory.on('click','.close-icon',function(e){
-            let itemArray = localStorage.getItem('searchHistory').match(regexp)[1].split(',')
-            let oneHistory = $(e.currentTarget).siblings('span').text()
-            // console.log(oneHistory)
-            let index = itemArray.indexOf(oneHistory)
-            $(e.currentTarget).parents('.item').remove()
-            if(index!==-1){itemArray.splice(index,1)}
+                </a>
+              </div>
+            </li>
+          `)
 
-            localStorage.setItem('searchHistory','['+itemArray+']')            
-            
-          })
-        }
-
-
-
-        //清空input值
-        $emptyInput.on('click',function(){
-          $searchBox.val('')
-          $syncSearch.text('')
-          $hotSearch.add($searchHistory).addClass('active')
-        })
-
-        $searchBox.on('input',function(e){
-          let value = e.target.value.trim()
-          if(value ===''){
-            $emptyInput.add($syncSearch).removeClass('active');
-            $hotSearch.add($searchHistory).addClass('active')
-            return
-          }else{
-            $emptyInput.addClass('active');
-            $hotSearch.add($searchHistory).removeClass('active')
-            
-            $h4 = $('<h4 class="search-what"></h4>').text(`搜索“${value}”`)
-            $syncSearch.html($h4).addClass('active')
-          
-            clearTimeout(timer)
-            timer = setTimeout(function() {
-              search(value).then(function(array){
-                displaySearch(array)
-              })
-            }, 1000);
+          //添加超清标记
+          if(ele.sq === true){
+            $li.find('.song-info').prepend($(`
+              <svg class="icon sq" aria-hidden="true">
+                <use xlink:href="#icon-sq"></use>
+              </svg>
+            `))
           }
+          
+          $musicList.append($li)
+        })
+        $hotListTab.append($musicList).find('.loading').remove()
+
+        $musicList.on('click','li',function(){
+          let index = $(this).index()
+          window.location.href = `./play.html?id=${index}`
         })
 
-        mockHotSearch(songDB)
-
+        //已加载
         $liContent.attr('data-isLoaded','yes')
-        function displaySearch(array){
-          let $musicList = $('<ul></ul>')
-          array.map(function(ele,index){
-            let $li = $(`
-              <li class="item" data-index= ${ele.id}>
-                <svg class="icon search-icon" aria-hidden="true">
-                  <use xlink:href="#icon-search"></use>
-                </svg>
-                <span>${ele.songName}</span>
-              </li>
-            `)
-            $musicList.append($li)
-          })
-          $syncSearch.append($musicList)
-         
-          $('.sync-search>ul').on('click','li',function(e){
-            window.location.href = `./play.html?id=${$(e.currentTarget).attr('data-index')}`
-          })
+
+        function orderNumber(index){
+          let number = index+1
+          if(Number(number)>0&&Number(number)<10){
+            return '0'+number
+          }else{
+            return ''+number
+          }
         }
-        function search(value){
-          return new Promise(function(resolve,reject){
-            $.get('./src/search.json').then(function(response){
-              let songDB = response
-              
-              let array = songDB.filter(function(ele,index){
-                for(key in ele){
-                  if(key!=='id' && ele[key].toString().toLowerCase().indexOf(value.toLowerCase())!==-1){
-                    return true
-                  }
-                }
-              })
-              resolve(array)
+      })
+    //index=2 搜索歌曲
+    }else if(index ===2){
+      let $searchBox = $('.search input')
+      //定时器，input去抖
+      let timer
+      //清空输入框按钮
+      let $emptyInput = $('.search .x-icon')
+      let $hotSearch = $('.hot-search')
+      let $searchHistory = $('.search-history')
+      //搜索时实时匹配结果
+      let $syncSearch = $('.sync-search')
+      let $searchWhat = $('.search-what')
+      //localStorage搜索历史字符串转为数组匹配正则
+
+
+      //清空input值
+      $emptyInput.on('click',function(){
+        $searchBox.val('')
+        $syncSearch.text('')
+        $hotSearch.add($searchHistory).addClass('active')
+      })
+
+      //input事件，见stackoverflow
+      $searchBox.on('input',function(e){
+        let value = e.target.value.trim()
+        //如果value为空，不显示清空按钮及搜索匹配
+        //反之显示
+        if(value ===''){
+          $emptyInput.add($syncSearch).removeClass('active');
+          $hotSearch.add($searchHistory).addClass('active')
+          return
+          
+        }else{
+          $emptyInput.addClass('active');
+          $hotSearch.add($searchHistory).removeClass('active')
+          
+          //显示搜索的值
+          $h4 = $('<h4 class="search-what"></h4>').text(`搜索“${value}”`)
+          $syncSearch.html($h4).addClass('active')
+          
+          //input事件计时1s后，触发
+          clearTimeout(timer)
+          //从数据库搜索func search，并将结果展示出来func displaySearch
+          timer = setTimeout(function() {
+            search(value).then(function(array){
+              displaySearch(array)
+            })
+          }, 1000);
+        }
+      })
+
+      //阻止form默认事件，储存查询历史
+      $('.search form').keypress(function(e){
+        if(e.keyCode==13){
+          let value = $searchBox.val()
+          e.preventDefault();
+          
+          generateHistory(value)
+        }
+      })
+
+      mockHotSearch()
+      //1.首次加载时 2.按回车时 3.点击热门搜索时 4.点击实时生成的匹配时 
+      generateHistory()
+      
+
+      //标记已加载过
+      $liContent.attr('data-isLoaded','yes')
+
+      function generateHistory(value){
+        let regexp = /^\[(.*)\]$/
+          
+        if(localStorage.getItem('searchHistory')===null||!(regexp.test(localStorage.getItem('searchHistory')))){
+          localStorage.setItem('searchHistory','[]')
+        }
+        let item = localStorage.getItem('searchHistory')
+        let itemArray = item.match(/^\[[\,]*(.*)\]$/)[1].split(',')
+        
+        if(value !==''&&value!==undefined&&itemArray.indexOf(value)===-1){
+          itemArray.push(value)
+          // console.log(itemArray)
+          localStorage.setItem('searchHistory',`[${itemArray.toString()}]`)  
+        }
+        console.log(localStorage.getItem('searchHistory'),2)
+        //生成搜索历史的dom及事件
+        let $ul = $('<ul></ul>')
+        if(itemArray.length < 1) return;
+        itemArray.map(function(ele){
+          if(ele==='')return
+          let $li = `
+            <li class="item">
+              <svg class="icon history-icon" aria-hidden="true">
+                <use xlink:href="#icon-history"></use>
+              </svg>
+              <div class="history">
+                  <span>${ele}</span>
+                  <svg class="icon close-icon" aria-hidden="true">
+                    <use xlink:href="#icon-close"></use>
+                  </svg>
+              </div>
+            </li>
+          `
+          $ul.append($li)
+        })
+        $searchHistory.html($ul)
+        //事件：删除历史
+        $searchHistory.find('.close-icon').on('click',function(e){
+          e.stopPropagation()
+          let itemArray = localStorage.getItem('searchHistory').match(regexp)[1].split(',')
+          let oneHistory = $(e.currentTarget).siblings('span').text()
+          // console.log(oneHistory)
+          let index = itemArray.indexOf(oneHistory)
+          $(e.currentTarget).parents('.item').remove()
+          if(index!==-1){itemArray.splice(index,1)}
+          localStorage.setItem('searchHistory','['+itemArray+']')
+        })
+        $searchHistory.find('ul').on('click','li',function(e){
+          // console.log(e.currentTarget)
+          // return
+          let songName = $(e.currentTarget).text().trim()
+          $.get('/src/search.json').then(function(response){
+            let songDB = response
+            songDB.map(function(ele){
+              if(ele.songName === songName){
+                window.location.href = `./play.html?id=${ele.id}`
+                return
+              }
             })
           })
-        }
-        function mockHotSearch(songDB){
-          let DB = songDB
-          let array = []
+        })
+      }
+
+      function displaySearch(array){
+        let $musicList = $('<ul></ul>')
+        array.map(function(ele,index){
+          let $li = $(`
+            <li class="item" data-index= ${ele.id}>
+              <svg class="icon search-icon" aria-hidden="true">
+                <use xlink:href="#icon-search"></use>
+              </svg>
+              <span>${ele.songName}</span>
+            </li>
+          `)
+          $musicList.append($li)
+        })
+        $syncSearch.append($musicList)
+        
+        $('.sync-search>ul').on('click','li',function(e){
+          a = $(e.currentTarget).text()
+          let value = $(e.currentTarget).text().trim()
+          generateHistory(value)
+          location.href = `./play.html?id=${$(e.currentTarget).attr('data-index')}`
+        })
+      }
+
+      function search(value){
+        return new Promise(function(resolve,reject){
+          $.get('./src/search.json').then(function(response){
+            let songDB = response
+            
+            let array = songDB.filter(function(ele,index){
+              for(key in ele){
+                if(key!=='id' && ele[key].toString().toLowerCase().indexOf(value.toLowerCase())!==-1){
+                  return true
+                }
+              }
+            })
+            resolve(array)
+          })
+        })
+      }
+      //mock热门搜索
+      function mockHotSearch(){
+        let DB
+        let array = []
+        
+        //mock热门搜索
+        $.get('//owf5g9dnv.bkt.clouddn.com/songsDB.json?v=20170922',function(response){
+          DB = response
+
+          //mock 6个0-9不重复随机数
           while (array.length < 6) {
             let number = Math.floor(Math.random()*10)
             if(array.indexOf(number) === -1){
               array.push(number)
             }
           }
+
           let $ul = $('<ul></ul>')
           array.map(function(ele){
-            songDB.map(function(el){
+            DB.map(function(el){
               if(el.id === ele){
                 let $li = $(`
                 <li class="item"><a href="./play.html?id=${el.id}">${el.songName}</a></li>               
@@ -287,10 +339,14 @@ $(function(){
               }
             })
           })
+          $ul.on('click','li',function(e){
+            //innerText取的值有回车符
+            let value = $(e.currentTarget).text().trim()
+            generateHistory(value)
+          })
           $hotSearch.append($ul)
-
-        } 
-      }
-    })
+        })
+      } 
+    }
   })
 })
--- src/js/playlist.js
+++ src/js/playlist.js
@@ -2,22 +2,53 @@ $(function(){
   let search = window.location.search
   let rId = Number(search.match(/\?rid=(\d+)/)[1])
   
-  $.get('/src/playlist.json/list1.json').then(function(response){
+  $.get(`/src/playlist.json/list${rId+1}.json`).then(function(response){
     let listInfo = response
     
-    $('.playlist-header').css('background-image',`url(${listInfo.rCoverUrl})`)
+    $('.playlist-header .background-img').css('background-image',`url(${listInfo.rCoverUrl})`)
     $('.playlist-header .cover').css('background-image',`url(${listInfo.rCoverUrl})`)
     let $viewTimes = $('.playlist-header .views')
     $viewTimes.text(listInfo.rPlayTimes)
 
+    //歌单名及作者
     let $titleInfo = $('.playlist-header .main')
-    $titleInfo.html(`
+    $html1 = `
       <p class="title">${listInfo.rTitle}</p>
       <div class="editor">
         <img class="editor-photo" src="${listInfo.rCoverUrl}" class="img"></img>
         <p class="name">${listInfo.rEditor}</p>
       </div>
+    `
+    $titleInfo.append($html1)
+
+    //标签及简介
+    let $playlistIntro = $('.playlist-intro')
+    let labels = listInfo.rLabel
+    let $labelList = $(`<ul class="label-list"></ul>`)
+    labels.forEach(function(el){
+      $li = $(`<li>${el}</li>`)
+      $labelList.append($li)
+    })
+    $html2 = $(`
+      <div class="label">
+        <p>标签：</p>
+        ${$labelList[0].outerHTML}
+      </div>
+      <p class="brief-info active">
+          简介：${listInfo.rBriefInfo.replace(/\\n/g,' <br> ')}
+      </p>
+      <svg class="icon down" aria-hidden="true">
+          <use xlink:href="#icon-down"></use>
+      </svg>
     `)
+    
+    //没用？？
+    // $html2.find('div').append($labelList)
+    
+    $playlistIntro.append($html2)
+    $('.playlist-intro').find('.brief-info').on('click',function(e){
+      $(e.currentTarget).toggleClass('active')
+    })
   })
 
   $.get('/src/hotlist.json').then(function(response){
@@ -62,8 +93,5 @@ $(function(){
     })
   })
   
-  let $briefInfo = $('.brief-info')
-  $briefInfo.on('click',function(e){
-    $briefInfo.toggleClass('active')
-  })
+  
 })
